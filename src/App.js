bd6adefa3d75c82e1fa76e51f00654d62db55c5d
Aaliyan Kapadia <aaliyan.kap@Outlook.com>
2023-03-08 01:13:11 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 import Prototype from "./components/pages/Prototype";
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  HashRouter,
+  Switch,
+  Route,
+} from "react-router-dom";
 import Layout from "./layout/Layout";
 import Home from "./components/pages/Home";
 import AboutUs from "./components/pages/AboutUs";
@@ -9,15 +14,24 @@ import AboutUs from "./components/pages/AboutUs";
 function App() {
   return (
     <div>
-      <Router>
+      <HashRouter>
         <Layout>
           <Switch>
-            <Route path={"/AboutUs"} component={AboutUs}></Route>
-            <Route path={"/Prototype"} component={Prototype}></Route>
-            <Route path={"/"} component={Home}></Route>
+            {/* <Route path="/" component={Home}></Route>
+            <Route path="/AboutUs" component={AboutUs}></Route>
+            <Route path="/Prototype" component={Prototype}></Route> */}
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/AboutUs">
+              <AboutUs />
+            </Route>
+            <Route path="/Prototype">
+              <Prototype />
+            </Route>
           </Switch>
         </Layout>
-      </Router>
+      </HashRouter>
     </div>
   );
 }
